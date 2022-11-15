7da24efbbcac4ab08ad2c0c3030b88289ff3e87d
ChunFuY <zxc3209999@gmail.com>
2022-11-16 07:17:26 +0800

    fixed some bugs

--- Assets/Scripts/BulletBehavior.cs
+++ Assets/Scripts/BulletBehavior.cs
@@ -4,33 +4,13 @@ using UnityEngine;
 
 public class BulletBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(gameObject.transform.position.y < -5 || gameObject.transform.position.x > 7 || gameObject.transform.position.x < -10 )
-        {
-            Destroy(gameObject);
-        }
-    }
-        
     private void OnTriggerEnter2D(Collider2D collision)
-    {
-        
-        //if((gameObject.transform.position.x > 7 || gameObject.transform.position.x < -10 ||
-        //gameObject.transform.position.y > 3 || gameObject.transform.position.y < -5) || 
-        //(collision.gameObject.tag == "Islands"))
+    {       
         if(collision.gameObject.tag == "Islands")
         {
+            Debug.Log(collision);
             Destroy(gameObject);
         }
-        
-        //Destroy(gameObject);
 
     }
     
